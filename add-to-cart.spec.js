24ebc95bae8c657c13f4dcbbcef577155ecd153b
Jessica Gouvea <59367286+jessicagouveah@users.noreply.github.com>
2024-08-06 11:07:53 -0300

    Create add-to-cart.spec.js

---
+++ add-to-cart.spec.js
@@ -0,0 +1,22 @@
+describe('Adicionar produto ao carrinho no site da Riachuelo', () => {
+  it('Deve adicionar um produto ao carrinho com sucesso', () => {
+    // Acesse a página inicial da Riachuelo
+    cy.visit('https://www.riachuelo.com.br/');
+
+    // Pesquise um produto específico (por exemplo, "camisa")
+    cy.get('input[name="search"]').type('camisa{enter}');
+
+    // Selecione o primeiro produto da lista de resultados
+    cy.get('.product-item').first().click();
+
+    // Verifique se a página do produto foi carregada
+    cy.url().should('include', '/produto/');
+
+    // Adicione o produto ao carrinho
+    cy.get('button[data-testid="add-to-cart-button"]').click();
+
+    // Verifique se o produto foi adicionado ao carrinho
+    cy.get('a[href="/carrinho"]').click();
+    cy.get('.cart-item').should('have.length.greaterThan', 0);
+  });
+});
